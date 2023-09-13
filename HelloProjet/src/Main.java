394dd71de0604a25e12764bca0b086cbec6058f7
BAQAQ <andy5201899@163.com>
2023-09-13 13:02:32 +0800

    添加文件到主

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("wadaxiwa");
         System.out.println("toligaduo");
+        System.out.println("toligad11111555uo");
+        System.out.println("toligad11111555uo9999");
     }
 }
